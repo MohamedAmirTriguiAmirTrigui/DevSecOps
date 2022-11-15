742c4cd8e5b6f6204ee7c49d9fefaad164fd080b
MohamedAmir Trigui <73851512+MohamedAmirTriguiAmirTrigui@users.noreply.github.com>
2022-11-15 23:16:41 +0100

    Update trivy-docker-image-scan.sh

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -17,11 +17,3 @@ docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit
     else
         echo "Image scanning passed. No CRITICAL vulnerabilities found"
     fi;
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
